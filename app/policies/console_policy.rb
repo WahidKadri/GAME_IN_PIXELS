2285b591507e30f90ecb5220e187bcc3f2ca05ef
Wahid Kadri <wahidkadri.motiondesigner@gmail.com>
2018-05-22 10:54:12 +0200

    defining routes & installing pundit

---
+++ app/policies/console_policy.rb
@@ -0,0 +1,7 @@
+class ConsolePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'consoles#home'
+  resources :consoles, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:new, :create]
+    resources :reviews ,only: [:new, :create]
+  end
+  get "/dashboards", to: "dashboards#bk_owner", as: :dash_owner
+  get "/dashboards", to: "dashboards#bk_renter", as: :dash_renter
 end
