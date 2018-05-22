d6c561b8a07b5e70e2140f74410673a9fae61c4b
hugo sola <hugo.degeorges@gmail.com>
2018-05-22 14:46:48 +0200

    latestreviewview

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,11 +2,15 @@ class ReviewsController < ApplicationController
 
   def new
     @review = Review.new
+    authorize @review
+    @console = Console.find(params[:console_id])
   end
 
   def create
+    @console = Console.find(params[:console_id])
     @review = Review.new(review_params)
     @review.console = @console
+    authorize @review
     if @review.save
       redirect_to consoles_path
     else
--- app/policies/review_policy.rb
+++ app/policies/review_policy.rb
@@ -1,7 +1,17 @@
 class ReviewPolicy < ApplicationPolicy
+
+  def new?
+    record.booking.user == user
+    # current_user
+  end
+
+  def create?
+    new?
+  end
+
   class Scope < Scope
     def resolve
       scope
     end
-  end
+end
 end
